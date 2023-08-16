9ce9c628ef70d897d7a8c4d104e164223d8a9027
Vasilisa Kirjanova <lisakirjanova097@gmail.com>
2023-08-16 17:35:13 +0300

    update logo&icon

--- src/components/ui/header/HeaderLogo.tsx
+++ src/components/ui/header/HeaderLogo.tsx
@@ -1,10 +1,9 @@
 import { FC } from "react";
-import pizzaLogo from "../../../../public/pizza-logo.svg";
 
 const HeaderLogo: FC = () => {
   return (
     <div className="header__logo">
-      <img width="38" src={pizzaLogo} alt="Pizza logo" />
+      <img width="38" src="/pizza.logo.svg" alt="Pizza logo" />
       <div>
         <h1>React Pizza</h1>
         <p>найсмачніша піца у Всесвіті</p>
