cfd94d85dad4cd4a2666759f050c0f4459e52343
Vasilisa Kirjanova <lisakirjanova097@gmail.com>
2023-08-17 20:48:39 +0300

    add PizzaItem component

---
+++ src/components/ui/pizza/PizzaOptions.tsx
@@ -0,0 +1,37 @@
+import { FC, useState } from "react";
+
+interface IPizzaOptions {
+  types: string[];
+  sizes: number[];
+}
+
+const PizzaOptions: FC<IPizzaOptions> = ({ types, sizes }) => {
+  const [activeType, setActiveType] = useState("тонке");
+  const [activeSize, setActiveSize] = useState(0);
+  return (
+    <div className="pizza-block__selector">
+      <ul>
+        {types.map((opt, i) => (
+          <li
+            key={i}
+            className={activeType === opt ? "active" : ""}
+            onClick={() => setActiveType(opt)}>
+            {opt}
+          </li>
+        ))}
+      </ul>
+      <ul>
+        {sizes.map((opt, i) => (
+          <li
+            key={i}
+            className={activeSize === i ? "active" : ""}
+            onClick={() => setActiveSize(i)}>
+            {opt} см
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default PizzaOptions;
