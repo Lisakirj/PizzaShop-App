78497c7c74e636f9a6942f915ce81cd1c2288cf4
Vasilisa Kirjanova <lisakirjanova097@gmail.com>
2023-08-17 16:39:05 +0300

    add categories data

---
+++ src/mockData/categories.ts
@@ -0,0 +1,10 @@
+import { Category } from "../types/category";
+
+export const categories: Category[] = [
+  { id: 1, name: "Всі" },
+  { id: 2, name: "М'ясна" },
+  { id: 3, name: "Вегетаріанська" },
+  { id: 4, name: "Гриль" },
+  { id: 5, name: "Гостра" },
+  { id: 6, name: "Закрита" },
+];
