6bf533c7c41a41a4dec1ba904a3726cb4c5433dd
Vasilisa Kirjanova <lisakirjanova097@gmail.com>
2023-08-23 20:18:06 +0300

    add SkeletonPizza component

--- src/components/page/PizzaListPage/PizzaListPage.tsx
+++ src/components/page/PizzaListPage/PizzaListPage.tsx
@@ -1,19 +1,24 @@
 import { FC, useEffect, useState } from "react";
 import axios from "axios";
+
 import PizzaItem from "../../ui/pizza/PizzaItem";
+import SkeletonPizza from "../../ui/pizza/SkeletonPizza";
 import { IPizzaItem } from "../../../types/pizzaItem";
+// import httpService from "../../../services/http.service";
+import config from "../../../config.json";
+
+axios.defaults.baseURL = config.apiEndPoint; // // // // // // !!!!!!
 
 const PizzaListPage: FC = () => {
   const [items, setItems] = useState<IPizzaItem[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const { data } = await axios.get(
-          `https://64e1055b50713530432ce695.mockapi.io/items`
-        );
-        console.log(data);
+        const { data } = await axios.get(`items`);
         setItems(data as IPizzaItem[]);
+        setIsLoading(false);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
@@ -25,9 +30,9 @@ const PizzaListPage: FC = () => {
     <>
       <h2 className="content__title">Всі піци</h2>
       <div className="content__items">
-        {items.map((item) => (
-          <PizzaItem pizza={item} key={item.id} />
-        ))}
+        {isLoading
+          ? [...new Array(6)].map((_, i) => <SkeletonPizza key={i} />)
+          : items.map((item) => <PizzaItem pizza={item} key={item.id} />)}
       </div>
     </>
   );
---
+++ src/components/ui/pizza/SkeletonPizza.tsx
@@ -0,0 +1,23 @@
+import { FC } from "react";
+import ContentLoader from "react-content-loader";
+
+const SkeletonPizza: FC = () => {
+  return (
+    <ContentLoader
+      className="pizza-block"
+      speed={2}
+      width={285}
+      height={465}
+      viewBox="0 0 285 465"
+      backgroundColor="#f3f3f3"
+      foregroundColor="#ecebeb">
+      <rect x="4" y="266" rx="10" ry="10" width="280" height="27" />
+      <circle cx="139" cy="124" r="118" />
+      <rect x="3" y="319" rx="10" ry="10" width="280" height="79" />
+      <rect x="5" y="425" rx="10" ry="10" width="85" height="33" />
+      <rect x="149" y="415" rx="30" ry="30" width="136" height="49" />
+    </ContentLoader>
+  );
+};
+
+export default SkeletonPizza;
