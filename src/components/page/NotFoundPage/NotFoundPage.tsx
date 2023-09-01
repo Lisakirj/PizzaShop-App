f35280d75f337d498928f077a648e6524b2327ee
Vasilisa Kirjanova <lisakirjanova097@gmail.com>
2023-09-01 12:53:35 +0300

    add NotFoundPage responsivity

--- src/components/page/NotFoundPage/NotFoundPage.tsx
+++ src/components/page/NotFoundPage/NotFoundPage.tsx
@@ -1,19 +1,25 @@
 import { FC } from "react";
 
-import style from "./NotFound.module.scss";
+// import style from "./NotFound.module.scss";
 
 const NotFoundPage: FC = () => {
   return (
-    <div className={style.root}>
-      <h1>
-        <span>😔</span>
-        <br />
-        Нічого не знайдено
-      </h1>
-      <p className={style.description}>
-        на жаль, дана сторінка відсутня у нашому інтернет-магазині
-      </p>
-    </div>
+    <section className="not-found">
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-12 col-lg-9 d-flex  flex-column justify-content-center align-items-center">
+            <h1 className="text-center mb-3">
+              <span>😔</span>
+              <br />
+              Нічого не знайдено
+            </h1>
+            <p className="text-sm-center">
+              на жаль, дана сторінка відсутня у нашому інтернет-магазині
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
