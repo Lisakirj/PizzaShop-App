07e03ac64dba4765be3ca664633613eb57580ced
Vasilisa Kirjanova <lisakirjanova097@gmail.com>
2023-09-15 15:14:18 +0300

    add cartSlice

--- src/store/slices/cartSlice.ts
+++ src/store/slices/cartSlice.ts
@@ -1,34 +1,116 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import type { RootState } from "../store";
+import type { RootState, AppDispatch } from "../store";
+import { cartItem } from "../../types/cartItem";
+import { calcTotalPrice } from "../../utils/helpers/calcTotalPrice";
+
+import { customAlphabet } from "nanoid";
+import { createSelector } from "@reduxjs/toolkit";
+
+type activeItems = {
+  type: string;
+  size: number;
+};
 
 interface ICartState {
-  value: number;
-  isLoading: boolean;
+  items: cartItem[];
+  activeType: string;
+  activeSize: number;
+  totalPrice: number;
 }
 
 const initialState: ICartState = {
-  value: 0,
-  isLoading: true,
+  items: [],
+  activeType: "",
+  activeSize: 0,
+  totalPrice: 0,
 };
-
 export const cartSlice = createSlice({
   name: "cart",
   initialState,
   reducers: {
-    cartRequested: (state) => {
-      state.isLoading = true;
+    cartItemAdded: (state, action: PayloadAction<cartItem>) => {
+      // console.log("payload", action.payload);
+      const findItem = state.items.find(
+        (obj) =>
+          obj.title === action.payload.title &&
+          obj.type === action.payload.type &&
+          obj.size === action.payload.size
+      );
+      if (findItem) {
+        findItem.count++;
+      } else {
+        state.items.push({
+          ...action.payload,
+          id: +customAlphabet("123456789", 3)(),
+        });
+      }
+      state.totalPrice = calcTotalPrice(state.items);
     },
-    cartRequestFailed: (state, action: PayloadAction<unknown>) => {
-      state.isLoading = false;
-      // state.errors = action.payload;
+
+    activeItemsSet: (state, action: PayloadAction<activeItems>) => {
+      state.activeType = action.payload.type;
+      state.activeSize = action.payload.size;
+    },
+
+    cartItemRemoved: (state, action: PayloadAction<number>) => {
+      state.items = state.items.filter((item) => item.id != action.payload);
+      state.totalPrice = calcTotalPrice(state.items);
+    },
+    cartItemDecreased: (state, action: PayloadAction<number>) => {
+      const item = state.items.find((obj) => obj.id === action.payload);
+      if (item && item.count > 1) {
+        item.count--;
+      } else {
+        state.items = state.items.filter((item) => item.id != action.payload);
+      }
+      state.totalPrice = calcTotalPrice(state.items);
+    },
+    cartCleared: (state) => {
+      state.items = [];
+      state.totalPrice = 0;
     },
   },
 });
 
 const { actions, reducer: cartReducer } = cartSlice;
-const { cartRequested, cartRequestFailed } = actions;
+const {
+  cartItemAdded,
+  cartCleared,
+  cartItemRemoved,
+  cartItemDecreased,
+  activeItemsSet,
+} = actions;
 
-//getters
-const getCartItems = () => (state: RootState) => state.cart.value;
+export const setActiveItems =
+  (items: activeItems) => (dispatch: AppDispatch) => {
+    dispatch(activeItemsSet(items));
+  };
+export const addCartItem = (item: cartItem) => (dispatch: AppDispatch) => {
+  dispatch(cartItemAdded(item));
+};
+export const removeCartItem = (id: number) => (dispatch: AppDispatch) => {
+  console.log(id);
+  dispatch(cartItemRemoved(id));
+};
+export const clearCart = () => (dispatch: AppDispatch) => {
+  dispatch(cartCleared());
+};
+export const decreaseCartItem = (id: number) => (dispatch: AppDispatch) => {
+  dispatch(cartItemDecreased(id));
+};
 
+//selectors
+export const getCartItems = () => (state: RootState) => state.cart.items;
+
+export const getActiveType = () => (state: RootState) => state.cart.activeType;
+export const getActiveSize = () => (state: RootState) => state.cart.activeSize;
+
+export const getTotalPrice = () => (state: RootState) => state.cart.totalPrice;
+export const getTotalCount = () => (state: RootState) =>
+  state.cart.items.reduce((total, item) => total + item.count, 0);
+
+export const getCartItemByTitle = (title: string) =>
+  createSelector([(state: RootState) => state.cart.items], (cartItems) => {
+    return cartItems.filter((obj) => obj.title === title);
+  });
 export default cartReducer;
---
+++ src/types/cartItem.ts
@@ -0,0 +1,9 @@
+export type cartItem = {
+  id: number;
+  title: string;
+  imageUrl: string;
+  price: number;
+  size: number;
+  type: string;
+  count: number;
+};
