cbd202780d66a6a4af8f5dc24b7908659e55b93e
Vasilisa Kirjanova <lisakirjanova097@gmail.com>
2023-09-20 19:00:22 +0300

    add redux-persist

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,22 @@
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { BrowserRouter } from "react-router-dom";
+import { StrictMode } from "react";
 
-import { store } from "./store/store.ts";
 import App from "./App.tsx";
+
+import configureAppStore from "./store/store.ts";
+const { store, persistor } = configureAppStore();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter>
+        <StrictMode>
+          <App />
+        </StrictMode>
+      </BrowserRouter>
+    </PersistGate>
   </Provider>
 );
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,26 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 import filterReducer from "./slices/filterSlice";
 import pizzasReducer from "./slices/pizzasSlice";
 import cartReducer from "./slices/cartSlice";
 import paginationReducer from "./slices/paginationSlice";
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["cart", "filter"],
+};
 
 const rootReducer = combineReducers({
   filter: filterReducer,
@@ -12,10 +29,25 @@ const rootReducer = combineReducers({
   pagination: paginationReducer,
 });
 
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
   devTools: process.env.NODE_ENV !== "production",
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+const configureAppStore = () => {
+  const persistor = persistStore(store);
+  return { store, persistor };
+};
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+
+export default configureAppStore;
