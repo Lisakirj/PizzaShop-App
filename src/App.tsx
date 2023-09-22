8c7a6e5b1b093c843e344e7eeac9ae75fa5b4211
Vasilisa Kirjanova <lisakirjanova097@gmail.com>
2023-09-22 14:16:45 +0300

    add code splitting with lazy()

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,43 @@
-import "./assets/scss/app.scss";
+import "./styles/app.scss";
+import { lazy, Suspense } from "react";
 
 import { Routes, Route } from "react-router-dom";
 
-import MainLayout from "./components/layouts/MainLayout";
-import Main from "./pages/Main";
-import Cart from "./pages/Cart";
-import NotFound from "./pages/NotFound";
+import MainLayout from "./layouts/MainLayout";
+import Home from "./pages/Home";
+
+const Cart = lazy(() => import("./pages/Cart"));
+const NotFound = lazy(() => import("./pages/NotFound"));
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<MainLayout />}>
-        <Route path="/" element={<Main />} />
-        <Route path="cart" element={<Cart />} />
-        <Route path="*" element={<NotFound />} />
+        <Route path="" element={<Home />} />
+        <Route
+          path="cart"
+          element={
+            <Suspense
+              fallback={
+                <div className="text-center p-5">
+                  Йде завантаження корзини..
+                </div>
+              }>
+              <Cart />
+            </Suspense>
+          }
+        />
+        <Route
+          path="*"
+          element={
+            <Suspense
+              fallback={
+                <div className="text-center p-5">Йде завантаження..</div>
+              }>
+              <NotFound />
+            </Suspense>
+          }
+        />
       </Route>
     </Routes>
   );
